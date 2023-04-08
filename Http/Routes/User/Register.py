8f764529cbb33bd54696cbb861ba94f6419eb822
JONYANDUNH <jonyandunh@outlook.com>
2023-04-08 18:09:28 +0800

    哈希密码

--- Http/Routes/User/Register.py
+++ Http/Routes/User/Register.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter
 
 from Utility.MongoDB import UsersDB
 from Utility.Models.User import UserModel
-
+from passlib.context import CryptContext
 router = APIRouter()
-
-
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 @router.post("/")
 async def Register(user: UserModel):
+    user.password=pwd_context.hash(user.password)
     result = UsersDB.insert_one(user.dict())
     return str(result.inserted_id)
